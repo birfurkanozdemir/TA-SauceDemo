f23449f354c319e1d4e1a3f3e633d867275ef2d7
birfurkanozdemir <birfurkanozdemir@gmail.com>
2023-07-29 23:28:32 +0300

    Added random text and take screenshot. General maintenance

--- src/test/java/LoginProcess.java
+++ src/test/java/LoginProcess.java
@@ -1,11 +1,18 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class LoginProcess {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
         System.setProperty("webdriver.chrome.driver", ".\\src\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -24,11 +31,22 @@ public class LoginProcess {
         else
             System.out.println("Title not verified");
 
-        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("demo"); //username: standard_user
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("demo123"); //password: secret_sauce
-        driver.findElement(By.xpath("//input[@value='Login']")).click();
-        driver.findElement(By.xpath("//h3[text()='Epic sadface: Username and password do not match any user in this service']"));
+        String username = UUID.randomUUID().toString();
+        System.out.println("Random username: " + username);
 
-        driver.close();
+        String password = UUID.randomUUID().toString();
+        System.out.println("Random password: " + password);
+
+        driver.findElement(By.id("user-name")).sendKeys(username); //username: standard_user
+        driver.findElement(By.cssSelector("#password")).sendKeys(password); //password: secret_sauce
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+        driver.findElement(By.xpath("//h3[contains(text(),'Username and password do not match')]"));
+
+        File screenShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File img = new File(".\\src\\images\\ss.png");
+        FileUtils.copyFile(screenShot, img);
+
+        //driver.close();
+        driver.quit();
     }
 }
